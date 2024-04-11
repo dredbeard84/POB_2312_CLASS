d2edbe2acfd3e6429ea0e2695388fd50b997f90b
NikSi039 <sidorova9999@internet.ru>
2024-04-11 20:59:04 +0200

    22

---
+++ Thema22 исключения/Program.cs
@@ -0,0 +1,25 @@
+﻿// Тема 22
+
+// Урок 28
+
+
+//int n;
+//do
+//{
+//    try
+//    {
+//        Console.Write("Введите число: ");
+//        n = int.Parse(Console.ReadLine());
+//        break;
+//    }
+//    catch(Exception ex)
+//    {
+//        Console.WriteLine(ex.Message);
+//    }
+//}
+//while (true);
+
+
+// ПРАКТИКА 2
+
+// # 3
