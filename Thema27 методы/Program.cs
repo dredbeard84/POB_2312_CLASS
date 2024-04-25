58c3ed30a06934b908395c2bf33da91704f0d911
NikSi039 <sidorova9999@internet.ru>
2024-04-25 22:10:18 +0200

    27

---
+++ Thema27 методы/Program.cs
@@ -0,0 +1,50 @@
+﻿// Урок 33. Методы
+
+//Person person1 = new Person();
+//class Person
+//{
+//    string FIO;
+//    int age;
+//    double rost;
+//    double weight;
+//}
+
+
+// ПРАКТИКА 1
+
+// # 1 калькулятор
+
+//double x, y;
+//Console.Write("Введите х: ");
+//double.TryParse(Console.ReadLine(), out x);
+//Console.Write("Введите y: ");
+//double.TryParse(Console.ReadLine(), out y);
+//Calculator calc = new Calculator(x, y);
+//Console.WriteLine(calc.Add());
+//Console.WriteLine(calc.Sub());
+//Console.WriteLine(calc.Mult());
+//Console.WriteLine(calc.Div());
+//class Calculator
+//{
+//    private double X;
+//    private double Y;
+//    public Calculator(double x, double y)
+//    {
+//        X = x;
+//        Y = y;
+//    }
+//    public double Add() => X + Y;// лямбда-выражение => заменяет return
+//    public double Sub()
+//        { return X - Y; }// предложила автозамена
+//    public double Mult()
+//        {
+//            return X * Y;
+//        }
+//    public double Div()
+//        {
+//            return X / Y;
+//        }
+//}
+
+
+// # 2 строковый анализатор
