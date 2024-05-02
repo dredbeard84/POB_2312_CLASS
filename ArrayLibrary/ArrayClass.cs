aa8f3f82ce5b4279093138665c0352eee918de6d
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-05-02 20:16:30 +0200

    библиотека

--- ArrayLibrary/ArrayClass.cs
+++ ArrayLibrary/ArrayClass.cs
@@ -1,20 +1,24 @@
 ﻿namespace ArrayLibrary
 {
-    public class ArrayClass<T> // <T> - дженерик
+    public class ArrayClass//<T> // <T> - дженерик
     {
-        private T[]? mas;// T - универсальный параметр
-        public ArrayClass(T[]? mas)
+//        private object[]? mas;// T - универсальный параметр
+
+        public ArrayClass(object[]? mas)
         {
             this.mas = mas;
         }
         public void Fill(int low, int high)
         {
             Random random = new Random();
-            if (mas[0].GetType() == typeof(int))
+            if (mas[0].GetType() == typeof(string))
             {
                 for (int i = 0; i < mas.Length; i++)
                 {
-                    mas[i] = (int)random.Next(low, high);
+                    string res = String.Empty;
+                    for(int j = 0; j < high; j++)
+                        res += (char)random.Next(60, 127);
+                    mas[i] = res;
                 }
             }
         }
