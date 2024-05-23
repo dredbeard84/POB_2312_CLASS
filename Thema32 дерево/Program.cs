06c606178c75b6cd2eb51ce174b083c752fa5ca7
NikSi039 <sidorova9999@internet.ru>
2024-05-23 22:06:36 +0200

    32

--- Thema32 дерево/Program.cs
+++ Thema32 дерево/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.Write(@"Введите путь: ");// C:\Users\C1NB3\Desktop
+
+PrintDirectory(new DirectoryInfo(Console.ReadLine()!), "", true);
+
+void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+{
+    Console.Write(indent);
+    if (lastDirectory)
+    {
+        Console.Write("'--");
+        indent += " ";
+    }
+    else
+    {
+        Console.Write("'--");
+        indent += "| ";
+    }
+    Console.WriteLine(directory.Name);
+    DirectoryInfo[] subDirectories = directory.GetDirectories();
+    for (int i = 0; i < subDirectories.Length; i++)
+    {
+        PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+    }
+}
