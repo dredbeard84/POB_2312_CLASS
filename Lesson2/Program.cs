59f16c12f85a148f8815bdd848d43b5c0e46b763
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-05-25 10:48:16 +0200

    2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,20 +1,17 @@
 ﻿// Тема 2. Обзор среды разработки. Структура программы и отладка
 
 
-Console.Write("Введите первое число: ");
-double a = double.Parse(Console.ReadLine());
-
-Console.Write("Введите второе число: ");
-double b = double.Parse(Console.ReadLine());
-
-double s = a + b;
-
-Console.WriteLine(a + "+" + b + "=" + s);
-Console.WriteLine("Сумма равна: " + s);
+//Console.Write("Введите 1-е число: ");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("Введите 2-е число: ");
+//double b = double.Parse(Console.ReadLine());
+//double s = a + b;
+//Console.WriteLine(a + " + " + b + " = " + s);
+//Console.WriteLine("Сумма: " + s);
 
 // Выбор преподавателя
-Console.WriteLine($"{a}+{b}={s}");
-Console.WriteLine($"Сумма равна: {s}");
+//Console.WriteLine($"{a} + {b} = {s}");
+//Console.WriteLine($"Сумма: {s}");
 
-Console.WriteLine("{0}+{1}={2}", a, b, s);
-Console.WriteLine("Сумма равна: {0}", s);
+//Console.WriteLine("{0} + {1} = {2}", a, b, s);
+//Console.WriteLine("Сумма: {0}", s);
